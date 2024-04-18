59e30894a88b51372fdb7e5c6ab03343da346fae
StanescuI <162870985+StanescuI@users.noreply.github.com>
2024-04-18 19:43:50 +0300

    Additional game modes
    Added 3 game modes and implemented the score for each one;
    Cleaner and shorter code

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 // Global variables and constants
 
-let airplane = document.getElementById("airplane")
-let gameSpace = document.getElementById("gameSpace")
-let isPressed = {'ArrowLeft' : false, 'ArrowRight' : false}
-let horizontalPos = 330
-let scoreIncrease
-let obstacleCreation
-let heightDecrease
-let isMoving
-let isPaused = true
 const RIGHT_BORDER = 650
 const BOTTOM_PIXELS = 550
 const COLLIDING_AREA = 500
 const OBJECT_WIDTH = 50
+let airplane = document.getElementById("airplane")
+let gameSpace = document.getElementById("gameSpace")
+let isPressed = {"ArrowLeft" : false, "ArrowRight" : false, "ArrowUp" : false}
+let horizontalPos = 350
+let bulletWidth = 10
+let bulletHeight = 15
+let activeBullets = 0
+let scoreIncrease
+let timeScore = 0
+let dodgeScore = 0
+let shootScore = 0
+let obstacleCreation
+let heightDecrease
+let isMoving
+let isPaused = true
 
 // Game states
 
+function gameType(nr) {
+	if (nr === 1) {
+		timeScore = 1
+	} else if (nr === 2) {
+		dodgeScore = 1
+	} else {
+		shootScore = 1
+	}
+}
+
 function startGame() {
 	document.getElementById("startButton").remove()
+	document.getElementById("startButton2").remove()
+	document.getElementById("startButton3").remove()
 	let infoBar = document.getElementById("infoBar")
-	let pauseButton = document.createElement('button')
+	let pauseButton = document.createElement("button")
 	pauseButton.id = "pauseGame"
 	pauseButton.textContent = "Pause"
 	pauseButton.onclick = pauseGame
 	infoBar.appendChild(pauseButton)
 	document.addEventListener("keydown", checkKeyPressed)
 	document.addEventListener("keyup", stopMoving)
-	document.addEventListener('visibilitychange', pauseTheGame)
+	document.addEventListener("visibilitychange", pauseTheGame)
 	pauseGame();
 }
 
@@ -53,8 +71,8 @@ function gameOver() {
 	document.getElementById("pauseGame").remove()
 	let infoBar = document.getElementById("infoBar")
 	let result = document.getElementById("scoreText")
-	result.textContent = `Game over. Your score is `
-	let restart = document.createElement('button')
+	result.textContent = "Game over. Your score is "
+	let restart = document.createElement("button")
 	restart.id = "pauseGame"
 	restart.textContent = "Restart"
 	restart.onclick = reloadPage
@@ -62,10 +80,8 @@ function gameOver() {
 }
 
 function pauseTheGame() {
-    if (document.visibilityState === 'hidden') {
-        if (!isPaused) {
-            pauseGame()
-        }
+    if (document.visibilityState === "hidden" && !isPaused) {
+        pauseGame()
     }
 }
 
@@ -74,13 +90,21 @@ function reloadPage() {
 }
 
 // Game updates
+function shootObstacle() {
+	let bullet = document.createElement("div")
+	bullet.id = `${Math.random() * 5}`
+	bullet.classList.add("bullets")
+	bullet.style.left = `${horizontalPos + (OBJECT_WIDTH / 2 - bulletWidth / 2)}px`
+	bullet.style.top = `${COLLIDING_AREA}px`
+	gameSpace.appendChild(bullet)
+	++activeBullets
+}
 
 function createObstacle() {
-	console.log("obstacle spawned")
 	let obstacleSpawn = Math.random() * RIGHT_BORDER
 	let obstacleVerticalPosition = 50
-	let obstacle = document.createElement('div')
-	obstacle.id = `${obstacleSpawn}`
+	let obstacle = document.createElement("div")
+	obstacle.id = obstacleSpawn
 	obstacle.className = "obstacle"
 	obstacle.style.top = `${obstacleVerticalPosition}px`
 	obstacle.style.left = `${obstacleSpawn}px`
@@ -88,7 +112,7 @@ function createObstacle() {
 }
 
 function decreaseHeight() {
-    let obstacles = document.querySelectorAll('.obstacle')
+    let obstacles = document.querySelectorAll(".obstacle")
     obstacles.forEach(function(obstacle) {
         let height = obstacle.style.top
         height = height.slice(0, -2)
@@ -98,6 +122,9 @@ function decreaseHeight() {
             obstacle.style.top = `${height}px`
         } else {
             obstacle.remove()
+			let scoreString = document.getElementById("score")
+			let currentScore = Number(scoreString.innerText)
+			scoreString.innerText = `${currentScore + dodgeScore}`
         }
 		let obstaclePos = obstacle.style.left
 		obstaclePos = obstaclePos.slice(0, -2)
@@ -110,44 +137,78 @@ function decreaseHeight() {
 			gameOver()
 		}
     });
+	let bullets = document.querySelectorAll(".bullets")
+	bullets.forEach(function(bullet) {
+		let bulletPos = bullet.style.top
+		bulletPos = bulletPos.slice(0, -2)
+		bulletPos = Number(bulletPos);
+		let bulletLeft = bullet.style.left
+		bulletLeft = bulletLeft.slice(0, -2)
+		bulletLeft = Number(bulletLeft);
+		if (bulletPos > 0 + OBJECT_WIDTH) {
+			bulletPos -= 1
+			bullet.style.top = `${bulletPos}px`
+		} else {
+			bullet.remove()
+			--activeBullets
+		}
+		let obstacles = document.querySelectorAll(".obstacle")
+		obstacles.forEach(function(obstacle) {
+			let height = obstacle.style.top
+        	height = height.slice(0, -2)
+        	height = Number(height);
+			let width = obstacle.style.left
+			width = width.slice(0, -2)
+			width = Number(width);
+			if (bulletPos > height && bulletPos < height + OBJECT_WIDTH &&
+				bulletLeft > width && bulletLeft < width + OBJECT_WIDTH ) {
+					bullet.remove()
+					--activeBullets
+					obstacle.remove()
+					let scoreString = document.getElementById("score")
+				let currentScore = Number(scoreString.innerText)
+				scoreString.innerText = `${currentScore + shootScore}`
+				}
+		})
+	});
 }
 
 function movePlayer() {
-	if (!isPaused){
-		if (isPressed['ArrowLeft'] && horizontalPos > 0 && !isPaused) {
-			horizontalPos += -5
-		}
-		if (isPressed['ArrowRight'] && horizontalPos < RIGHT_BORDER && !isPaused) {
-			horizontalPos += 5
-		}
-		airplane.style.left = `${horizontalPos}px`
+	if (isPressed["ArrowLeft"] && horizontalPos > 0 && !isPaused) {
+		horizontalPos += -5
 	}
+	if (isPressed["ArrowRight"] && horizontalPos < RIGHT_BORDER && !isPaused) {
+		horizontalPos += 5
+	}
+	airplane.style.left = `${horizontalPos}px`
 }
 
 function checkKeyPressed(e) {
-    switch(e.key) {
-        case 'ArrowLeft':
-            isPressed['ArrowLeft'] = true
-            break;
-        case 'ArrowRight':
-            isPressed['ArrowRight'] = true
-            break;
-    }
+    if(e.key === "ArrowLeft") {
+        isPressed["ArrowLeft"] = true
+	}
+	if (e.key === "ArrowRight") {
+		isPressed["ArrowRight"] = true
+	}
+	if (e.key === "ArrowUp") {
+		if (activeBullets < 3 && shootScore === 1) {
+			isPressed["ArrowUp"] = true
+			shootObstacle()
+		}
+	}
 }
 
 function stopMoving(e) {
-    switch(e.key) {
-        case 'ArrowLeft':
-            isPressed['ArrowLeft'] = false
-            break;
-        case 'ArrowRight':
-            isPressed['ArrowRight'] = false
-            break;
-    }
+    if(e.key === "ArrowLeft") {
+        isPressed["ArrowLeft"] = false
+	}
+	if (e.key === "ArrowRight") {
+		isPressed["ArrowRight"] = false
+	}
 }
 
 function increaseScore() {
 	let scoreString = document.getElementById("score")
-	score = Number(scoreString.innerText)
-	scoreString.innerText = `${++score}`
+	let currentScore = Number(scoreString.innerText)
+	scoreString.innerText = `${currentScore + timeScore}`
 }
