912688d644bbc91fe71269c52be70f198c19c198
StanescuI <162870985+StanescuI@users.noreply.github.com>
2024-05-25 21:36:37 +0300

    Cleaner code
    Removed redundant parameters;
    Isolated conditional structure in a separate function for more code clarity

--- script.js
+++ script.js
@@ -167,19 +167,21 @@ function bulletCollision(bullet, bulletPos, bulletLeft) {
         let width = obstacle.style.left;
         width = width.slice(0, -2);
         width = Number(width);
-        isColliding(bullet, bulletPos, bulletLeft, height, width, obstacle)
+        if (isColliding(bulletPos, bulletLeft, height, width,)) {
+            bullet.remove();
+            --activeBullets;
+            obstacle.remove();
+            let scoreString = document.getElementById("score");
+            let currentScore = Number(scoreString.innerText);
+            scoreString.innerText = `${currentScore + shootScore}`;
+        }
     })
 }
 
-function isColliding(bullet, bulletPos, bulletLeft, height, width, obstacle) {
+function isColliding(bulletPos, bulletLeft, height, width,) {
     if (bulletPos > height && bulletPos < height + OBJECT_WIDTH &&
         bulletLeft > width && bulletLeft < width + OBJECT_WIDTH) {
-        bullet.remove();
-        --activeBullets;
-        obstacle.remove();
-        let scoreString = document.getElementById("score");
-        let currentScore = Number(scoreString.innerText);
-        scoreString.innerText = `${currentScore + shootScore}`;
+        return true;
     }
 }
 
